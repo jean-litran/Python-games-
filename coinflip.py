e7068373f212e073540bfdcb478d9b585b52015f
Jean Litran calegari <86577414+jean-litran@users.noreply.github.com>
2023-04-04 20:00:17 -0300

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -1,20 +1,45 @@
 import random
+
 print("I will flip a coin 1000 times. Guess how many times it will come up! Heads (Press enter to begin)")
 input()
-filps = 0
+
+flips = 0
 heads = 0
-#mecanismo pra jogar a moeda
+
+# mecanismo para jogar a moeda
 while flips < 1000:
     if random.randint(0,1) == 1: 
-        heads = heads +1
-        flips = flips + 1
+        heads += 1
+    flips += 1
     if flips == 900:
-        print("900 flips an there have been" +str(heads) + 'Heads')
+        print("900 flips and there have been " + str(heads) + " Heads")
     if flips == 100:
-                print('At 100 tosses, heads has come up' +str (heads) +'times so far')
+        print("At 100 tosses, heads has come up " + str(heads) + " times so far")
     if flips == 500:
-                    print('Halfway Done and heads has come up'+str(heads)+'times.')
-                    
+        print("Halfway done and heads has come up " + str(heads) + " times.")
+
 print()
-print("Out of 1000 coins tosses has came up" +str(heads)+'times')
-print('Were you close!')
+print("Out of 1000 coin tosses, heads came up " + str(heads) + " times.")
+print("Were you close?")import random
+
+print("I will flip a coin 1000 times. Guess how many times it will come up! Heads (Press enter to begin)")
+input()
+
+flips = 0
+heads = 0
+
+# mecanismo para jogar a moeda
+while flips < 1000:
+    if random.randint(0,1) == 1: 
+        heads += 1
+    flips += 1
+    if flips == 900:
+        print("900 flips and there have been " + str(heads) + " Heads")
+    if flips == 100:
+        print("At 100 tosses, heads has come up " + str(heads) + " times so far")
+    if flips == 500:
+        print("Halfway done and heads has come up " + str(heads) + " times.")
+
+print()
+print("Out of 1000 coin tosses, heads came up " + str(heads) + " times.")
+print("Were you close?")
